4c0358ff1474cc6dff45b88e5e88ba641775aa0e
putrafauzan32@gmail.com <email@example.com>
2024-11-10 12:36:27 +0700

    update api

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import swaggerUi from "swagger-ui-express";
-import fs from 'fs/promises';
+import fs from "fs/promises";
 import path from "path";
 import authRoutes from "./routes/authRoutes.js";
 
@@ -16,42 +16,50 @@ app.use(express.json());
 
 // Menggunakan path relatif dari root proyek untuk Swagger JSON
 async function loadSwaggerDoc() {
-    try {
-        const swaggerPath = path.join(process.cwd(), 'swagger', 'swagger.json');
-        console.log(`Loading Swagger document from: ${swaggerPath}`);
-        const swaggerDocument = JSON.parse(await fs.readFile(swaggerPath, 'utf-8'));
-        console.log('Swagger document loaded successfully.');
+  try {
+    const swaggerPath = path.join(process.cwd(), "swagger", "swagger.json");
+    console.log(`Loading Swagger document from: ${swaggerPath}`);
+    const swaggerDocument = JSON.parse(await fs.readFile(swaggerPath, "utf-8"));
+    console.log("Swagger document loaded successfully.");
 
-        const options = {
-            swaggerOptions: {
-                url: `https://chatter-api.vercel.app/swagger/swagger.json`,
-            },
-            customCssUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.3/swagger-ui.css"
-        };
+    const options = {
+      swaggerOptions: {
+        url: `https://chatter-api.vercel.app/swagger/swagger.json`,
+      },
+      customCssUrl:
+        "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.3/swagger-ui.css",
+    };
 
-        app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
-    } catch (error) {
-        console.error('Error loading Swagger document:', error);
-    }
+    app.use(
+      "/api-docs",
+      swaggerUi.serve,
+      swaggerUi.setup(swaggerDocument, options)
+    );
+  } catch (error) {
+    console.error("Error loading Swagger document:", error);
+  }
 }
 
 // Mengatur route API utama
-app.use('/api', authRoutes);
+app.use("/api", authRoutes);
 
 // Memanggil fungsi untuk memuat dokumen Swagger
 loadSwaggerDoc();
 
 // Koneksi ke MongoDB
-mongoose.connect(process.env.MONGODB_URI)
-    .then(() => {
-        console.log("Connected to MongoDB");
-    })
-    .catch((error) => {
-        console.error("Error connecting to MongoDB:", error);
-    });
-
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 // Memulai server
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
