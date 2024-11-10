9b008a648bb6742df7496f0ef20bd170a463aed6
putrafauzan32@gmail.com <email@example.com>
2024-11-10 11:34:08 +0700

    update api

--- app.js
+++ app.js
@@ -43,13 +43,14 @@ app.use('/api', authRoutes);
 loadSwaggerDoc();
 
 // Koneksi ke MongoDB
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("Connected to MongoDB");
     })
     .catch((error) => {
-        console.log("Error connecting to MongoDB:", error);
+        console.error("Error connecting to MongoDB:", error);
     });
+
 
 // Memulai server
 app.listen(PORT, () => {
